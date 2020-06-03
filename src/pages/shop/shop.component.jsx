7764e42e3bb8c5def0b4af78d3330626b902ab8a
Yadhu Krishnan <yadhuk94@gmail.com>
2020-06-03 22:40:37 +0530

    Folder 11

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,26 +1,12 @@
-import React, { Component } from "react";
-import { SHOP_DATA } from "./shop.data";
-import CollectionPreview from "../../components/preview-collection/collection-preview.component";
+import React from "react";
+import CollectionsOverview from "../../components/collections-overview/collections-overview.component";
 
-export class ShopPage extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      collections: SHOP_DATA,
-    };
-  }
-
-  render() {
-    const { collections } = this.state;
-    return (
-      <div className="shop-page">
-        {collections.map(({ id, ...otherCollectionProps }) => (
-          <CollectionPreview key={id} {...otherCollectionProps} />
-        ))}
-      </div>
-    );
-  }
-}
+const ShopPage = () => {
+  return (
+    <div className="shop-page">
+      <CollectionsOverview />
+    </div>
+  );
+};
 
 export default ShopPage;
