adbb9ee6bf63976c8c9cf52f4df989e2672a6864
Yadhu Krishnan <yadhuk94@gmail.com>
2020-06-03 21:00:56 +0530

    implemented persistence for cart

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,10 @@
 import logger from "redux-logger";
 import { createStore, applyMiddleware } from "redux";
+import { persistStore } from "redux-persist";
 import rootReducer from "./root-reducer";
 
 const middlewares = [logger];
 
-const store = createStore(rootReducer, applyMiddleware(...middlewares));
+export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
-export default store;
+export const persistor = persistStore(store);
